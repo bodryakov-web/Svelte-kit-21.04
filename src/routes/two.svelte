1a1f1d4891f62d72121110e51c2c14126d2498a2
pgtk-web <pgtk.web@gmail.com>
2021-04-21 13:09:40 +0600

    q

---
+++ src/routes/two.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+
+</script>
+
+<svelte:head>
+	<title>Вторая</title>
+</svelte:head>
+
+<div class="content">
+	<h1>Вторая</h1>
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
